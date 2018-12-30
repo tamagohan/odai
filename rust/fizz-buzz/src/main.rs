a414221955dd2c305a95bfac29376d6ba90d2fbc
tamagohan <fantastipo23@gmail.com>
2018-12-30 18:20:51 +0900

    Implement FizzBuzz

--- rust/fizz-buzz/src/main.rs
+++ rust/fizz-buzz/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    puts_fizz_buzz(100);
+}
+
+fn puts_fizz_buzz(max: i32) -> () {
+    (1..max)
+        .map(|num| fizz_buzz(num))
+        .for_each(|word| println!("{}", word));
+}
+
+fn fizz_buzz(num: i32) -> String {
+    if num % 15 == 0 {
+        "FizzBuzz".to_string()
+    } else if num % 3 == 0 {
+        "Fizz".to_string()
+    } else if num % 5 == 0 {
+        "Buzz".to_string()
+    } else {
+        num.to_string()
+    }
 }
