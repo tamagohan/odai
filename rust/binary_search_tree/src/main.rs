995effd3937a408b5a4f0bb7d7a98abad688a90d
tamagohan <fantastipo23@gmail.com>
2020-04-28 22:36:32 +0900

    Imp struct

--- rust/binary_search_tree/src/main.rs
+++ rust/binary_search_tree/src/main.rs
@@ -1,3 +1,20 @@
+pub struct BinarySearchTree<T>(BinarySearchTreeInner<T>);
+
+pub enum BinarySearchTreeInner<T> {
+    Nil,
+    Node {
+        val: T,
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+}
+
+impl<T> BinarySearchTree<T> {
+    pub fn new() -> Self {
+        Self(BinarySearchTreeInner::Nil)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
