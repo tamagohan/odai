9abfc7a66468f731f8f0bf786bb728b6e4971b9a
tamagohan <fantastipo23@gmail.com>
2020-04-29 18:12:57 +0900

    Imp add function

--- rust/binary_search_tree/src/main.rs
+++ rust/binary_search_tree/src/main.rs
@@ -1,6 +1,8 @@
-pub struct BinarySearchTree<T>(BinarySearchTreeInner<T>);
+#[derive(Debug)]
+pub struct BinarySearchTree<T: std::cmp::Ord + std::fmt::Debug>(BinarySearchTreeInner<T>);
 
-pub enum BinarySearchTreeInner<T> {
+#[derive(Debug)]
+enum BinarySearchTreeInner<T> {
     Nil,
     Node {
         val: T,
@@ -9,12 +11,43 @@ pub enum BinarySearchTreeInner<T> {
     },
 }
 
-impl<T> BinarySearchTree<T> {
+impl<T: std::cmp::Ord + std::fmt::Debug> BinarySearchTree<T> {
     pub fn new() -> Self {
         Self(BinarySearchTreeInner::Nil)
+    }
+
+    pub fn add(&mut self, val: T) {
+        let nil = Self::find_nil_to_add(&mut self.0, &val);
+        *nil = BinarySearchTreeInner::Node {
+            val: val,
+            left: Box::new(BinarySearchTreeInner::Nil),
+            right: Box::new(BinarySearchTreeInner::Nil),
+        };
+    }
+
+    fn find_nil_to_add<'t, 'v>(
+        cur_node: &'t mut BinarySearchTreeInner<T>,
+        val: &'v T,
+    ) -> &'t mut BinarySearchTreeInner<T> {
+        match cur_node {
+            BinarySearchTreeInner::Nil => cur_node,
+            BinarySearchTreeInner::Node {
+                val: cur_v,
+                left,
+                right,
+            } => {
+                if val <= cur_v {
+                    Self::find_nil_to_add(right, &val)
+                } else {
+                    Self::find_nil_to_add(left, &val)
+                }
+            }
+        }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut tree = BinarySearchTree::<i32>::new();
+    tree.add(5);
+    println!("{:?}", tree);
 }
